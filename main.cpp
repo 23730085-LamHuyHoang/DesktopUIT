61d5f7d372b9a9c636ff7d5307daddae9ad577b7
Lâm Huy Hoàng <23730085@ms.uit.edu.vn>
2024-04-25 00:20:21 +0700

    update code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include "kmin_console.h"
 using namespace std;
-#define DOT_RAN 79
+#define DOT_RAN 254
 #define MAX 100
 #define LEN 1
 #define XUONG 2
 #define TRAI 3
 #define PHAI 4
+#define TUONG_TREN 5
+#define TUONG_DUOI 25
+#define TUONG_TRAI 10
+#define TUONG_PHAI 100
+
 
 struct ToaDo
 {
@@ -15,12 +20,16 @@ struct ToaDo
 };
 
 ToaDo ran[MAX];
- int soDot = 3;// so luong phan tu
+    int soDot = 3;
 
- void khoiTaoRan();
-void hienthiran();
-void diChuyen(int huong);
+void khoiTaoRan();
+void hienThiRan(ToaDo dotCuoiCu);
+ToaDo diChuyen(int huong);
 void batSuKien(int &huong);
+void veTuong();
+bool kiemTraThua();
+void xuLyThua();
+
 int main()
 {
     /*for (int i = 0; i< 256; i++)
@@ -28,62 +37,70 @@ int main()
         cout << i << ": " << (char)i << endl;
     }
     return 0;*/
-khoiTaoRan();
-     // game loop
-     int x = 0, y = 0;
-     int huong = TRAI;
-     while(1)
-     {
+    khoiTaoRan();
 
-         clrscr();
-         diChuyen(huong);
-         hienthiran();
-         batSuKien(huong);
-        // gotoXY(x, y);
-        //cout << (char)DOT_RAN;
-        Sleep(500);
-        //x++;
-     }
+    int x = 0, y = 0;
+    int huong = PHAI;
+    veTuong();
+    while(1)
+    {
+        ToaDo dotCuoiCu = diChuyen(huong);
+        batSuKien(huong);
+        hienThiRan(dotCuoiCu);
+        Sleep(300);
+        if (kiemTraThua() == true)
+            break;
+    };
+    xuLyThua();
 }
+
 void khoiTaoRan()
 {
-    ran[0].x = 4;
-    ran[1].x = 5;
-    ran[2].x = 6;
-    ran[0].y = ran[1].y = ran[2].y = 1;
+    ran[0].x = TUONG_TRAI + 7;
+    ran[1].x = TUONG_TRAI + 6;
+    ran[2].x = TUONG_TRAI + 5;
+    ran[0].y = ran[1].y = ran[2].y = TUONG_TREN + 5;
 }
-void hienthiran()
+
+void hienThiRan(ToaDo dotCuoiCu)
 {
     for (int i = 0; i < soDot; i++)
     {
         gotoXY(ran[i].x, ran[i].y);
         cout << (char)DOT_RAN;
     }
-
+    gotoXY(dotCuoiCu.x, dotCuoiCu.y);
+    cout << " ";
 }
-void diChuyen(int huong)
+
+ToaDo diChuyen(int huong)
 {
-    // dich qua phai 1 phan tu
+    ToaDo dotCuoiCu = ran[soDot - 1];
+
     for(int i = soDot - 1; i >= 1; i--)
     {
         ran[i] = ran[i - 1];
     }
+
     switch (huong)
     {
-  case LEN:
-    ran[0].y--;
-    break;
-    case XUONG:
-    ran[0].y++;
-    break;
-    case TRAI:
-    ran[0].x--;
-    break;
-    case PHAI:
-    ran[0].x++;
-    break;
+        case LEN:
+            ran[0].y--;
+            break;
+        case XUONG:
+            ran[0].y++;
+            break;
+        case TRAI:
+            ran[0].x--;
+            break;
+        case PHAI:
+            ran[0].x++;
+            break;
     }
+
+    return dotCuoiCu;
 }
+
 void batSuKien(int &huong)
 {
     int key = inputKey();
@@ -91,16 +108,48 @@ void batSuKien(int &huong)
     {
         huong = LEN;
     }
-        else if (key == KEY_DOWN)
-        {
-            huong = XUONG;
-        }
-        else if (key == KEY_LEFT)
-        {
-            huong = TRAI;
-        }
-        else if(key == KEY_RIGHT)
-        {
-            huong = PHAI;
-        }
+    else if (key == KEY_DOWN)
+    {
+        huong = XUONG;
+    }
+    else if (key == KEY_LEFT)
+    {
+        huong = TRAI;
+    }
+    else if(key == KEY_RIGHT)
+    {
+        huong = PHAI;
+    }
+}
+
+void veTuong()
+{
+    for (int i = TUONG_TRAI; i <= TUONG_PHAI; i++)
+    {
+        gotoXY(i, TUONG_TREN);
+        cout << (char)223;
+        gotoXY(i, TUONG_DUOI);
+        cout << (char)220;
+    }
+    for (int i = TUONG_TREN; i <= TUONG_DUOI; i++)
+    {
+        gotoXY(TUONG_TRAI, i);
+        cout << (char)222;
+        gotoXY(TUONG_PHAI, i);
+        cout << (char)221;
+    }
+}
+
+bool kiemTraThua()
+{
+    if (ran[0].y == TUONG_TREN || ran[0].y == TUONG_DUOI || ran[0].x == TUONG_TRAI + 1 || ran[0].x == TUONG_PHAI - 1)
+        return true;
+    return false;
+}
+
+void xuLyThua()
+{
+    Sleep(1000);
+    clrscr();
+    cout << "Game Over!!!";
 }
